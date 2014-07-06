82a5bdde1520b443242a2db538ed7c9034a26551
fawda123 <mbeck@D2626XMBECK3.aa.ad.epa.gov>
2014-07-06 14:38:22 -0500

    revamp of weighted regression windows, only one for dec_time

--- sim_funs.r
+++ sim_funs.r
@@ -1217,54 +1217,37 @@ dec_fun <- function(dat_in, var_nm = 'DateTimeStamp'){
   }
 
 ######
-#function for getting regression weights
+# function for getting regression weights, only one vector
+# note that this subsets the input data frame for faster wt selection
+# subset is by limiting window for product of weights (dec_time)
+# subsetted weights are recombined to equal vector of length = nrow(dat_in)
 #'wt_vars' is name of three variables to weight
 #'ref_in' is row of dat.in that is used as reference
 #'dat_in' is data to get weights from
 #'wins' are the windows for the three wt.vars, values represent halves
 #'all' will return all weights, rather than the product of all three
-wt_fun<-function(ref_in, dat_in,
-  wt_vars = c('jday', 'hour', 'Tide'),
-  wins = list(2, 0.5, NULL),
-  all = F){
-  
-#   # for subroutine
-#   dat_in <- DO_sim
-#   ref_in <- dat_in[1,]
-#   wt_vars = c('jday', 'hour', 'dTide')
-#   wins = list(5, 0.23, 2.651631) #list(15, 0.5, NULL)
+#'slice' is logical for subsetting 'dat_in' for faster wt selection
+#'subs_only' is logical for returning only wt vectors that are non-zero
+wt_fun <- function(ref_in, dat_in,
+  win,
+  wt_var = 'dec_time',
+  slice = T, 
+  all = F, 
+  subs_only = F){
   
   # sanity check
-  if(sum(wt_vars %in% names(dat_in)) != length(wt_vars))
+  if(sum(wt_var %in% names(dat_in)) != length(wt_var))
     stop('Weighting variables must be named in "dat_in"')
   
-  # windows for each of three variables
-  wins_1<-wins[[1]]
-  wins_2<-wins[[2]]
-  wins_3<-wins[[3]]
-  
-  # default window width for third variable is half its range
-  if(is.null(wins[[3]])) wins_3 <- diff(range(dat_in[, wt_vars[3]]))/2
-  
   # weighting tri-cube function
-  # mirror extends weighting function if vector repeats, e.g. monthly
   # 'dat_cal' is observation for weight assignment
   # 'ref' is reference observation for fitting the model
   # 'win' is window width from above (divided by two)
   # 'mirr' is logical indicating if distance accounts for repeating variables (e.g., month)
-  # 'scl_val' is range for the ref vector of obs, used to get correct distance for mirrored obs
-  wt_fun_sub <- function(dat_cal, ref, win, mirr = F, scl_val = 1){
+  wt_fun_sub <- function(dat_cal, ref, win){
     
     # dist_val is distance of value from the ref
     dist_val <- sapply(ref, function(x) abs(dat_cal - x))
-    
-    # repeat if distance is checked on non-continuous number line
-    if(mirr)
-      dist_val <- pmin(
-        dist_val, 
-        sapply(ref, function(x) abs(dat_cal - x + scl_val)),
-        sapply(ref, function(x) abs(dat_cal - scl_val + x))
-        )
     
     # get wts within window, otherwise zero
     win_out <- dist_val > win
@@ -1276,25 +1259,33 @@ wt_fun<-function(ref_in, dat_in,
     }
 
   #reference (starting) data
-  ref_1 <- as.numeric(ref_in[, wt_vars[1]])
-  ref_2 <- as.numeric(ref_in[, wt_vars[2]])
-  ref_3 <- as.numeric(ref_in[, wt_vars[3]])
+  ref_1 <- as.numeric(ref_in[, wt_var])
+
+  # window width
+  wins_1 <- win
+  
+  ##
+  # subset 'dat_in' by max window size for faster calc
+  # this is repeated if min number of wts > 0 is not met
+  # subset vector is all T if not using subset
+  dec_rng <- range(dat_in$dec_time)
+  ref_time <- unique(ref_in$dec_time)
+  dec_sub <- with(dat_in, 
+    dec_time > ref_time - wins_1 * 5 & dec_time < ref_time + wins_1 * 5
+    )
+  if(!slice) dec_sub <- rep(T, length = nrow(dat_in))
+  dat_sub <- dat_in[dec_sub, ]
 
   ##
   # weights for each observation in relation to reference
   # see comments for 'wt_fun_sub' for 'scl_val' argument
   
-  # jday
-  wts_1 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[1]]), 
-    ref = ref_1, win = wins_1, mirr = T, scl_val = 365) 
-  # hour
-  wts_2 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[2]]), 
-    ref = ref_2, win = wins_2, mirr = T, scl_val = 1)
-  # tide
-  wts_3 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[3]]), 
-    ref = ref_3, win = wins_3)
+  # dec_time weights
+  wts_1 <- wt_fun_sub(as.numeric(dat_sub[, wt_var]), 
+    ref = ref_1, win = wins_1) 
+
   # all as product 
-  out <- sapply(1:nrow(ref_in), function(x) wts_1[, x] * wts_2[, x] * wts_3[, x])
+  out <- wts_1
   
   gr_zero <- colSums(out > 0)
   #cat('   Number of weights greater than zero =',gr.zero,'\n')
@@ -1302,29 +1293,55 @@ wt_fun<-function(ref_in, dat_in,
   # extend window widths of weight vector is less than 100
   while(any(gr_zero < 100)){
     
+    # increase window size by 10%
     wins_1 <- 1.1 * wins_1
-    wins_2 <- 1.1 * wins_2
-    wins_3 <- 1.1 * wins_3 
+
+    # subset again
+    dec_sub <- with(dat_in, 
+      dec_time > ref_time - wins_1 * 5 & dec_time < ref_time + wins_1 * 5
+      )
+    if(!slice) dec_sub <- rep(T, length = nrow(dat_in))
+    dat_sub <- dat_in[dec_sub, ]
     
     #weights for each observation in relation to reference
-    wts_1 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[1]]), 
-      ref = ref_1, win = wins_1, mirr = T, scl_val = 365)
-    wts_2 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[2]]), 
-      ref = ref_2, win = wins_2, mirr = T, scl_val = 1)
-    wts_3 <- wt_fun_sub(as.numeric(dat_in[, wt_vars[3]]), 
-      ref = ref_3, win = wins_3)
+    wts_1 <- wt_fun_sub(as.numeric(dat_sub[, wt_var]), 
+      ref = ref_1, win = wins_1)
     
-    out <- sapply(1:nrow(ref_in), 
-      function(x) wts_1[, x] * wts_2[, x] * wts_3[, x])
+    out <- wts_1
     
     gr_zero <- colSums(out > 0)
     
     }
   
+  if(subs_only){
+    
+    nms <- which(dec_sub)
+    out <- alply(out, 2, function(x) {
+    
+      to_sel <- x > 0
+      tmp <- x[to_sel]
+      names(tmp) <- which(dec_sub)[to_sel]
+      tmp
+    
+      })
+    
+    return(out)
+    
+    }
+  
+  # extend weight vectors to length of dat_in
+  empty_mat <- matrix(0, ncol = nrow(ref_in), nrow = nrow(dat_in))
+  empty_fill <- function(wts_in) {
+    out <- empty_mat
+    out[dec_sub,] <- wts_in
+    out
+    }
+  out <- empty_fill(out)
+
   #return all weights if T
   if(all){
-    out <- list(wts_1, wts_2, wts_3)
-    names(out) <- wt_vars
+    out <- data.frame(dat_in$DateTimeStamp, out)
+    names(out) <- c('DateTimeStamp', 'wts')
     return(out)
     }
   
@@ -1527,7 +1544,7 @@ ts_create <- function(time_in, do.amp, tide_cat, tide_assoc, err_rng_obs,
   DO_sim$sunrise[day_vec > 6 & day_vec <= 18] <- -1
   
   # get tide if character
-  if(class(tide_cat) == 'character'){
+  if(grepl('character|factor', class(tide_cat))){
     if(tide_cat == 'Semidiurnal')
       waves.in <- list(c(12.42, 1)) # M2, principal lunar semidiurnal
     if(tide_cat == 'Diurnal') 
@@ -1584,7 +1601,7 @@ ts_create <- function(time_in, do.amp, tide_cat, tide_assoc, err_rng_obs,
 # 'dat_in' is raw data used to create 'grd_in' and used to get predictions
 # 'DO_obs' is string indicating name of col for observed DO values from 'dat_in'
 # output is data frame same as 'dat_in' but includes predicted and norm columns
-wtreg_fun <- function(dat_in, DO_obs = 'DO_obs', wins = list(4, 0.5, NULL),
+wtreg_fun <- function(dat_in, DO_obs = 'DO_obs', win = 10,
   parallel = F, progress = F){
 
   # get mean tidal height from empirical data
@@ -1614,8 +1631,7 @@ wtreg_fun <- function(dat_in, DO_obs = 'DO_obs', wins = list(4, 0.5, NULL),
         }
       
       # get wts
-      ref_wts <- wt_fun(ref_in, dat_in, wins = wins, 
-        wt_vars = c('jday', 'hour', 'Tide'))
+      ref_wts <- wt_fun(ref_in, dat_in, win = win)
   
       #OLS wtd model
       out <- lapply(1:ncol(ref_wts),
--- source_me.R
+++ source_me.R
@@ -19,6 +19,7 @@ setwd('M:/docs/SWMP/detiding_sim/')
 
 # functions to use
 source('sim_funs.r')
+
 tide_cat <- c('Diurnal', 'Semidiurnal', 'Mixed Semidiurnal')
 tide_cat <- factor(tide_cat, levels = tide_cat)
 bio_rng <- round(seq(0, 2, length = 3),2)
@@ -32,15 +33,15 @@ names(eval_grd) <- c('tide_cat', 'bio_rng', 'tide_assoc', 'err_rng_pro',
   'err_rng_obs')
 save(eval_grd, file = 'eval_grd.RData')
 
-wins_days <- seq(2, 6, length = 3)
-wins_hours <- seq(0.25, 0.75, length = 3)
-wins_tides <- seq(0.25, 0.75, length = 3)
-wins_grd <- expand.grid(wins_days, wins_hours, wins_tides)
-names(wins_grd) <- c('jday', 'hour', 'Tide')
+wins <- c(1, seq(5, 20, by = 5))
+
+wins_grd <- expand.grid(wins)
+names(wins_grd) <- c('dec_time')
 
 comb_grd <- expand.grid(tide_cat, bio_rng, tide_assoc, err_rng_pro, err_rng_obs,
-  wins_days, wins_hours, wins_tides)
+  wins)
 names(comb_grd) <- c(names(eval_grd), names(wins_grd))
+save(comb_grd, file = 'comb_grd.RData')
 
 # time vector
 vec <- c('2014-05-01 00:00:00', '2014-05-31 00:00:00')
@@ -77,10 +78,10 @@ res <- foreach(row = 1:nrow(comb_grd)) %dopar% {
       )  
     )
 
-  wins_in <- with(to_eval, list(jday, hour, Tide))
+  win_in <- with(to_eval, dec_time)
   
   # get results
-  res_tmp <- wtreg_fun(DO_sim, wins = wins_in, parallel = F)
+  res_tmp <- wtreg_fun(DO_sim, win = win_in, parallel = F)
   
   res_tmp
   
